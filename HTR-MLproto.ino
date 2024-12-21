40b0a8b68a9b2d17e8b123bdcde60a94c50038de
Jon Schwartz <jon@jon-schwartz.com>
2024-12-21 16:31:43 -0500

    feat: added hit the wall detection, chore: moved code out of loop into dedicated methods, fix: changed variable types to be more efficient, chore: moved repeated integers to declared constants, chore: refactored motor methods to be more DRY, feat: added protection for trying to spin motors in both directions at once

--- HTR-MLproto.ino
+++ HTR-MLproto.ino
@@ -1,18 +1,31 @@
 
-int LeftBumperStatus = 0;
-int RightBumperStatus = 0;
-int BumperBumped = 0;
-int LastBumperBumped = 0;
+#define RIGHT_MOTOR_FORWARD_PIN 3
+#define LEFT_MOTOR_FORWARD_PIN 9
+#define RIGHT_MOTOR_REVERSE_PIN 5
+#define LEFT_MOTOR_REVERSE_PIN 6
 
-int LastActionPerformed = 0;
+#define LEFT_BUMPER_PIN 10
+#define RIGHT_BUMPER_PIN 11
 
-int MEMORY[11][2][2] = {0};  //LastActionPerformed, LeftBumperStatus, RightBumperStatus
-int PreviousMemoryAction = 0;
-int PreviousLeftBumperStatus = 0;
-int PreviousRightBumperStatus = 0;
+#define SHORT_DURATION 100
+#define LONG_DURATION 500
+
+#define NOT_GOING_FORWARD_THRESHOLD 1250
+#define BUMPER_HIT_THRESHOLD 1500
+
+bool LeftBumperStatus = 0;
+bool RightBumperStatus = 0;
+byte BumperBumped = 0;
+byte LastBumperBumped = 0;
+
+byte LastActionPerformed = 0;
+
+byte MEMORY[11][2][2] = {0};  //LastActionPerformed, LeftBumperStatus, RightBumperStatus
+byte PreviousMemoryAction = 0;
+bool PreviousLeftBumperStatus = 0;
+bool PreviousRightBumperStatus = 0;
 
 bool BadRobot = false;
-bool ResumeState = true;
 
 unsigned long NotGoingForwardTimer = 0;
 unsigned long BumperHitTimer = 0;
@@ -26,143 +39,121 @@ void PerformAction(int action)
   {
     case 0:
       Resume();
-      ResumeState = true;
       break;
 
     case 1:
-      SpinLeft(100);
+      SpinLeft(SHORT_DURATION);
       break;
 
     case 2:
-      SpinRight(100);
+      SpinRight(SHORT_DURATION);
       break;
 
     case 3:
-      BackLeft(100);
+      BackLeft(SHORT_DURATION);
       break;
 
     case 4:
-      BackRight(100);
+      BackRight(SHORT_DURATION);
       break;
 
     case 5:
-      SpinLeft(500);
+      SpinLeft(LONG_DURATION);
       break;
 
     case 6:
-      SpinRight(500);
+      SpinRight(LONG_DURATION);
       break;
 
     case 7:
-      BackLeft(500);
+      BackLeft(LONG_DURATION);
       break;
 
     case 8:
-      BackRight(500);
+      BackRight(LONG_DURATION);
       break;
 
     case 9:
-      Backup(100);
+      Backup(SHORT_DURATION);
       break;
       
     case 10:
-      Backup(500);
+      Backup(LONG_DURATION);
       break;
   }
 }
+
 //MOVEMENT Functions
-void SpinLeft(int Duration)
+void MoveMotors(int Duration, bool right_forward, bool left_forward, bool right_reverse, bool left_reverse)
 {
-  digitalWrite(3,1);    //Right Motor Forward Pin
-  digitalWrite(9,0);    //Left Motor Forward Pin
-  digitalWrite(5,0);    //Right Motor Reverse Pin
-  digitalWrite(6,1);    //Left Motor Reverse Pin
+  //Don't try to spin both directions at the same time
+  if ((right_forward == right_reverse) && (right_forward == true))
+    return;
+  if ((left_forward == left_reverse) && (left_forward == true))
+    return;
+  
+  digitalWrite(RIGHT_MOTOR_FORWARD_PIN, right_forward);    //Right Motor Forward Pin
+  digitalWrite(LEFT_MOTOR_FORWARD_PIN, left_forward);    //Left Motor Forward Pin
+  digitalWrite(RIGHT_MOTOR_REVERSE_PIN, right_reverse);    //Right Motor Reverse Pin
+  digitalWrite(LEFT_MOTOR_REVERSE_PIN, left_reverse);    //Left Motor Reverse Pin
 
   delay(Duration);
+}
+
+void SpinLeft(int Duration)
+{
+  MoveMotors(Duration, 1, 0, 0, 1);
 }
 
 void SpinRight(int Duration)
 {
-  digitalWrite(3,0);    //Right Motor Forward Pin
-  digitalWrite(9,1);    //Left Motor Forward Pin
-  digitalWrite(5,1);    //Right Motor Reverse Pin
-  digitalWrite(6,0);    //Left Motor Reverse Pin
-
-  delay(Duration);
+  MoveMotors(Duration, 0, 1, 1, 0);
 }
 
 void BackRight(int Duration)
 {
-  digitalWrite(3,0);    //Right Motor Forward Pin
-  digitalWrite(9,0);    //Left Motor Forward Pin
-  digitalWrite(5,1);    //Right Motor Reverse Pin
-  digitalWrite(6,0);    //Left Motor Reverse Pin
-
-  delay(Duration);
+  MoveMotors(Duration, 0, 1, 0, 1);
 }
 
 void BackLeft(int Duration)
 {
-  digitalWrite(3,0);    //Right Motor Forward Pin
-  digitalWrite(9,0);    //Left Motor Forward Pin
-  digitalWrite(5,0);    //Right Motor Reverse Pin
-  digitalWrite(6,1);    //Left Motor Reverse Pin
-
-  delay(Duration);
+  MoveMotors(Duration, 0, 0, 0, 1);
 }
 
 void Backup(int Duration)
 {
-  digitalWrite(3,0);    //Right Motor Forward Pin
-  digitalWrite(9,0);    //Left Motor Forward Pin
-  digitalWrite(5,1);    //Right Motor Reverse Pin
-  digitalWrite(6,1);    //Left Motor Reverse Pin
-
-  delay(Duration);
+  MoveMotors(Duration, 0, 0, 1, 1);
 }
 
 void Resume()
 {
-  digitalWrite(3,1);    //Right Motor Forward Pin
-  digitalWrite(9,1);    //Left Motor Forward Pin
-  digitalWrite(5,0);    //Right Motor Reverse Pin
-  digitalWrite(6,0);    //Left Motor Reverse Pin
-
+  MoveMotors(0, 1, 1, 0, 0);
   NotGoingForwardTimer = millis();
 }
 
-void setup() 
+void UpdateBumperStatus()
 {
-  Serial.begin(9600);
-  pinMode (3,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(6,OUTPUT);
-  pinMode(9, OUTPUT);
+  LeftBumperStatus = digitalRead(LEFT_BUMPER_PIN);
+  RightBumperStatus = digitalRead(RIGHT_BUMPER_PIN);
 
-  pinMode(10, INPUT);
-  pinMode(11, INPUT);
-
+  if ((LeftBumperStatus == 1) && (RightBumperStatus == 1))
+    //Are we crashed into a wall head on?
+    BumperBumped = 3;
+  else if (LeftBumperStatus == 1)
+    BumperBumped = 0;
+  else if (RightBumperStatus == 1)
+    BumperBumped = 1;
 }
 
-void loop() 
+void IsBadRobot()
 {
-  LeftBumperStatus = digitalRead(10);
-  RightBumperStatus = digitalRead(11);
-
-  if (LeftBumperStatus == 1)
-    BumperBumped = 0;
-
-  if (RightBumperStatus == 1)
-    BumperBumped = 1;
-
   //Bad Robot Conditions
-
-  if (millis() - NotGoingForwardTimer > 1250)
+  if (millis() - NotGoingForwardTimer > NOT_GOING_FORWARD_THRESHOLD)
     BadRobot = true;
   
   if (LeftBumperStatus == 1 || RightBumperStatus == 1)
   {
-    if (millis() - BumperHitTimer < 1500)
+    if (millis() - BumperHitTimer < BUMPER_HIT_THRESHOLD)
       if (BumperBumped == LastBumperBumped)
         BadRobot = true;
 
@@ -173,7 +164,25 @@ void loop()
 
   if (PreviousMemoryAction == 0 && LeftBumperStatus == 0 && RightBumperStatus == 0 && MEMORY[LastActionPerformed][LeftBumperStatus][RightBumperStatus] != 0)
     BadRobot = true;
+}
 
+void Learn()
+{
+  //Learning
+  if (BadRobot)
+    MEMORY[PreviousMemoryAction][PreviousLeftBumperStatus][PreviousRightBumperStatus] = random(0, 10);
+
+  //Select Next Action 
+  PerformAction(MEMORY[LastActionPerformed][LeftBumperStatus][RightBumperStatus]);
+
+  //Update Previous Actions
+  PreviousMemoryAction = LastActionPerformed;
+  PreviousLeftBumperStatus = LeftBumperStatus;
+  PreviousRightBumperStatus = RightBumperStatus;
+}
+
+void OutputStats()
+{
   Serial.print(LeftBumperStatus);
   Serial.print(" X ");
   Serial.print(RightBumperStatus);
@@ -181,22 +190,24 @@ void loop()
   Serial.print(BadRobot);
   Serial.print(" MEM:");
   Serial.println(MEMORY[LastActionPerformed][LeftBumperStatus][RightBumperStatus]);
-  
+}
 
-  //Learning:
-  if (BadRobot)
-    MEMORY[PreviousMemoryAction][PreviousLeftBumperStatus][PreviousRightBumperStatus] = random(0,10);
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(RIGHT_MOTOR_FORWARD_PIN, OUTPUT);
+  pinMode(LEFT_MOTOR_FORWARD_PIN, OUTPUT);
+  pinMode(RIGHT_MOTOR_REVERSE_PIN, OUTPUT);
+  pinMode(LEFT_MOTOR_REVERSE_PIN, OUTPUT);
 
-  //Select Next Action 
-  PerformAction(MEMORY[LastActionPerformed][LeftBumperStatus][RightBumperStatus]);
+  pinMode(LEFT_BUMPER_PIN, INPUT);
+  pinMode(RIGHT_BUMPER_PIN, INPUT);
+}
 
-  
-
-
-  //Update Previous Actions
-  PreviousMemoryAction = LastActionPerformed;
-  PreviousLeftBumperStatus = LeftBumperStatus;
-  PreviousRightBumperStatus = RightBumperStatus;
-  
-
+void loop() 
+{
+  UpdateBumperStatus();
+  IsBadRobot();
+  OutputStats();
+  Learn();
 }
